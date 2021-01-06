36aabdc31cf4262cc4c7a7be367f9c47962d7dc9
Asif Ahmed <48513167+asif2305@users.noreply.github.com>
2021-01-06 22:48:27 +0100

    added new interface and class

---
+++ Creational Design Pattern/Factory  Design Pattern/src/Shape.java
@@ -0,0 +1,7 @@
+// https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
+//In Factory pattern, we create object without exposing the creation logic to the client and refer to newly created object using a common interface.
+public interface Shape {
+
+    void draw();
+
+}
